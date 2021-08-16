d961e2179070335bf63c45cf673c5172efc1b945
cyt0 <62910322+GrantParkinson@users.noreply.github.com>
2021-08-15 22:20:24 -0700

    Configured bootstrap react and created navbar(needs to be floated right)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import logo from './logo.svg';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+
+<script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>This commit did not work (jk)</p>
-        <img src="img/IMG_4072.jpg" className="" alt="grant with cat" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="www.hltv.org">Grant's portfolio site</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="www.hltv.org">Work</Nav.Link>
+              <Nav.Link href="www.hltv.org">Contact</Nav.Link>
+              <Nav.Link href="www.hltv.org">Blog</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div >
+
   );
 }
 
